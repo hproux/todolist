e9756d24dfadb09e3531db8afeef4cda5373fc69
gciliberti <g.ciliberti@protonmail.com>
2020-03-06 09:44:03 +0100

    ajout module photo

--- app/components/TodoCreate.vue
+++ app/components/TodoCreate.vue
@@ -10,8 +10,7 @@
         </StackLayout>
         <StackLayout v-if="type=='photo'">
             <TextField class="title" v-model:text="toDo.title" hint="Entrez le titre ..."/>
-            <TextView class="content" v-model:text="toDo.content" hint="Description ..."/>
-            <Button class="validate" text="Valider" @tap="saveToDoPhoto"/>
+            <Button class="validate" text="Prendre une photo" @tap="saveToDoPhoto"/>
         </StackLayout>
         <StackLayout v-if="type=='video'">
             <TextField class="title" v-model:text="toDo.title" hint="Entrez le titre ..."/>
@@ -25,6 +24,10 @@
 <script>
     import Todo from '../classes/Todo.js';
     import axios from "axios";
+    import { EventData, Observable, fromObject } from "tns-core-modules/data/observable";
+    import { Page } from "tns-core-modules/ui/page";
+    import { View } from 'tns-core-modules/ui/core/view';
+    import { takePicture, requestPermissions } from "nativescript-camera";
 
     export default {
 
@@ -33,6 +36,7 @@
         data() {
             return {
                 toDo: new Todo(),
+                cameraImage : null,
             }
         },
         created(){
@@ -57,11 +61,41 @@
                     alert("Tous les champs ne sont pas remplis!");
                 }
             },
-            saveToDoPhoto() {
+            saveToDoPhoto(args) {
                 console.log("photo");
-                if (this.toDo.title !== "" && this.toDo.content !== "") {
+                if (this.toDo.title !== "") {
+                        let page = (args.object).page;
+                        let that = this;
+                        requestPermissions().then(
+                            () => {
+                                takePicture({ width: 320, height: 240, keepAspectRatio: true, saveToGallery: false, allowsEditing: false }).
+                                then((imageAsset) => {
+                                        that.cameraImage = imageAsset;
+                                        imageAsset.getImageAsync(function (nativeImage) {
+                                            let scale = 1;
+                                            let actualWidth = 0;
+                                            let actualHeight = 0;
+                                            if (imageAsset.android) {
+                                                // get the current density of the screen (dpi) and divide it by the default one to get the scale
+                                                scale = nativeImage.getDensity() / android.util.DisplayMetrics.DENSITY_DEFAULT;
+                                                actualWidth = nativeImage.getWidth();
+                                                actualHeight = nativeImage.getHeight();
+                                            } else {
+                                                scale = nativeImage.scale;
+                                                actualWidth = nativeImage.size.width * scale;
+                                                actualHeight = nativeImage.size.height * scale;
+                                            }
+                                        });
+                                    },
+                                    (err) => {
+                                        console.log("Error -> " + err.message);
+                                    });
+                            },
+                            () => alert('permissions rejected')
+                        );
+
                 } else {
-                    alert("Tous les champs ne sont pas remplis!");
+                    alert("Le titre n'est pas rempli!");
                 }
             },
             saveToDoVideo() {
